d009ba553b299b1880db137fcaa30b6d9636a551
Md Najmul Haque <najmulhaque008@gmail.com>
2023-01-19 17:39:18 +0600

    medicine image updated

--- models/Medicine.js
+++ models/Medicine.js
@@ -10,17 +10,40 @@ const medicineSchema = mongoose.Schema({
         type: String,
         require: [true, 'Please enter generic name']
     },
+    medicineCategory: {
+        type: String,
+        require: true,
+        enum: {
+            value: ['Tablet', 'Capsule', 'Drop', 'Syrup', 'Ointment', 'Injection'],
+            message: "Medicine type can't be {VALUE}, must be tablet/capsule/drop/syrup/ointment/injection"
+        }
+    },
     supplierName: {
         type: String,
         require: [true, 'Please enter supplier name']
     },
-    type: {
+    brandName: {
         type: String,
-        require: true,
-        enum: {
-            value: ['tablet', 'capsule', 'drop', 'syrup', 'ointment', 'injection'],
-            message: "Medicine type can't be {VALUE}, must be tablet/capsule/drop/syrup/ointment/injection"
-        }
+        require: [true, 'Please enter brand name']
+    },
+    unit: {
+        type: String,
+        require: [true, 'Please enter measurement unit']
+    },
+    origin: {
+        type: String,
+        require: [true, 'Please enter country of origin']
+    },
+    lotControl: {
+        type: String,
+    },
+    quantity: {
+        type: Number,
+        require: [true, 'Please enter medicine quantity']
+    },
+    expData: {
+        type: Date,
+        require: [true, 'Please enter medicine expire date']
     },
     price: {
         type: Number,
@@ -31,6 +54,9 @@ const medicineSchema = mongoose.Schema({
         required: true,
         validate: [valid.isURL, "wrong url, please provide correct url"]
     }],
+    remarks: {
+        type: String,
+    }
 })
 
 const Medicine = mongoose.model('Medicine', medicineSchema);
