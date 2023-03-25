a43b861445e0de2060a70b7f361e9de89153d7b5
Md Najmul Haque <najmulhaque008@gmail.com>
2023-03-25 13:52:42 +0600

    category schema created

---
+++ models/Category.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+
+const categorySchema = mongoose.Schema({
+    catId: {
+        type: String,
+        required: [true, "Please provide a id"],
+        trim: true,
+    },
+    categoryName: {
+        type: String,
+        required: [true, "Please provide a category name"],
+        trim: true,
+        lowercase: true,
+        minLength: [3, "Name must be at least 3 characters."],
+        maxLength: [20, "Name is too large"],
+    },
+    image: [{
+        type: String,
+        required: true,
+        validate: [validator.isURL, "wrong url, please provide correct url"]
+    }],
+    status: {
+        type: String,
+        default: "active",
+        enum: ["active", "inactive"],
+    }
+}, {
+    timestamps: true
+})
+
+const Category = mongoose.model('Category', categorySchema);
+
+export default Category
